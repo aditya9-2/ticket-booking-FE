b62f1976cf4d33a0db7eaaf35b34bf792bf8a5cb
Aditya Basak <adityabasak7031@gmail.com>
2026-01-19 17:10:29 +0530

    add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 
 import './App.css'
-import Navbar from './components/Navbar'
+import AboutPage from './pages/AboutPage'
+import AllEventsPage from './pages/AllEventsPage'
 import SigninPage from './pages/auth/SigninPage'
 import { SignupPage } from './pages/auth/SignupPage'
+import ContactPage from './pages/ContactPage'
+import EventPage from './pages/EventPage'
 import HomePage from './pages/Homepage'
 import { Route, Routes } from "react-router"
 function App() {
@@ -10,11 +13,16 @@ function App() {
 
   return (
     <div className='min-h-screen'>
-      
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path='/signup' element={<SignupPage />} />
         <Route path='/signin' element={<SigninPage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/contact' element={<ContactPage />} />
+        <Route path='/events' element={<AllEventsPage />} />
+        <Route path='/event/:eventId' element={<EventPage />} />
+        
       </ Routes>
     </div>
   )
